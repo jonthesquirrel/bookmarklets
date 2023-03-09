5b22dccc1f0f841ddb7642b9515159842153ed34
Jonathan Herman <jdh11235@gmail.com>
2023-03-08 21:52:41 -0600

    Switch to arrow functions

--- CleanAmazonURL.js
+++ CleanAmazonURL.js
@@ -1,4 +1,4 @@
-javascript:(function(){
+javascript:(() => {
   const url = window.location.href;
   const newUrl = url.replace(/https?:\/\/www\.amazon\.com\/.*?\/dp/, 'https://www.amazon.com/dp').split('?')[0].replace(/\/$/, "");
   navigator.clipboard.writeText(newUrl);
