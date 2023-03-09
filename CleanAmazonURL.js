6e5cf88916b4d611e0b7c66afca701abf7f59e5f
Jonathan Herman <jdh11235@gmail.com>
2023-03-08 21:51:42 -0600

    Remove trailling slash from Amazon URLs

--- CleanAmazonURL.js
+++ CleanAmazonURL.js
@@ -1,5 +1,5 @@
 javascript:(function(){
   const url = window.location.href;
-  const newUrl = url.replace(/https?:\/\/www\.amazon\.com\/.*?\/dp/, 'https://www.amazon.com/dp').split('?')[0];
+  const newUrl = url.replace(/https?:\/\/www\.amazon\.com\/.*?\/dp/, 'https://www.amazon.com/dp').split('?')[0].replace(/\/$/, "");
   navigator.clipboard.writeText(newUrl);
 })();
