109bfbd42144c356f1cc42f8beb4ae27e781392a
Jonathan Herman <jdh11235@gmail.com>
2023-03-08 21:56:56 -0600

    Make work with URLs that end in /ref=whatever

--- CleanAmazonURL.js
+++ CleanAmazonURL.js
@@ -1,5 +1,5 @@
 javascript:(() => {
   const url = window.location.href;
-  const newUrl = url.replace(/https?:\/\/www\.amazon\.com\/.*?\/dp/, 'https://amazon.com/dp').split('?')[0].replace(/\/$/, "");
+  const newUrl = url.replace(/https?:\/\/www\.amazon\.com\/.*?\/dp/, 'https://amazon.com/dp').split('?')[0].replace(/\/[^/]*$/, '');
   navigator.clipboard.writeText(newUrl);
 })();
